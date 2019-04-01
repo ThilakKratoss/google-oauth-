0d78c036261db831ff6fe82a03dfc23a83073304
thilak <thilakal9299@gmail.com>
2019-04-01 22:25:27 +0530

    handles auth

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 const express =  require('express');
 const mongoose = require('mongoose');
+const passport = require('passport');
+//passport config
+
+require('./config/passport')(passport);
+
+
+//load routes
+const auth = require('./routes/auth');
 
 const app = express();
 
@@ -7,6 +15,12 @@ const app = express();
 app.get('/' , (req,res)=>{
     res.send('it works');
 });
+
+//use 
+
+app.use('/auth',auth);
+
+
 
 const port = process.env.PORT || 5500;
 
---
+++ config/keys.js
@@ -0,0 +1,6 @@
+module.exports = {
+    mongoURI:'mongo ds123675.mlab.com:23675/phoenicry -u thilak -p thilakal123',
+    googleClientID:'46597581385-mhq7l9jpb32s6fuefsbouhcptdoosljd.apps.googleusercontent.com',
+    googleClientSecret:'N1qEbJKU0gpnjrXcbWx9F78-'
+
+}
